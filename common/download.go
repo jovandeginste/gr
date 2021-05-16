a6cd667b10d8c72040600fb4aa8a2c7bc4078cfe
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2021-05-16 16:49:20 +0200

    Use mholt/archiver
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- common/download.go
+++ common/download.go
@@ -1,14 +1,11 @@
 package common
 
 import (
-	"archive/tar"
-	"compress/gzip"
-	"io"
 	"io/ioutil"
 	"os"
 	"path"
-	"path/filepath"
-	"strings"
+
+	archiver "github.com/mholt/archiver/v3"
 )
 
 func (a *Asset) DownloadTo(destination string) error {
@@ -34,6 +31,7 @@ func (a *Asset) DownloadTo(destination string) error {
 		return err
 	}
 
+	a.Logger.Infof("Unpacking '%s' to '%s'...", file, extractDir)
 	if err := unpack(file, extractDir); err != nil {
 		return err
 	}
@@ -42,80 +40,5 @@ func (a *Asset) DownloadTo(destination string) error {
 }
 
 func unpack(file, extractDir string) error {
-	unfile := strings.TrimSuffix(file, ".gz")
-	if err := ungzip(file, unfile); err != nil {
-		return err
-	}
-
-	return untar(unfile, extractDir)
-}
-
-func untar(tarball, target string) error {
-	reader, err := os.Open(tarball)
-	if err != nil {
-		return err
-	}
-	defer reader.Close()
-	tarReader := tar.NewReader(reader)
-
-	for {
-		header, err := tarReader.Next()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			return err
-		}
-
-		p := filepath.Join(target, header.Name)
-		info := header.FileInfo()
-
-		if info.IsDir() {
-			if err = os.MkdirAll(p, info.Mode()); err != nil {
-				return err
-			}
-
-			continue
-		}
-
-		file, err := os.OpenFile(p, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		if _, err = io.Copy(file, tarReader); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func ungzip(source, target string) error {
-	reader, err := os.Open(source)
-	if err != nil {
-		return err
-	}
-
-	defer reader.Close()
-
-	archive, err := gzip.NewReader(reader)
-	if err != nil {
-		return err
-	}
-
-	defer archive.Close()
-
-	target = filepath.Join(target, archive.Name)
-
-	writer, err := os.Create(target)
-	if err != nil {
-		return err
-	}
-
-	defer writer.Close()
-
-	_, err = io.Copy(writer, archive)
-
-	return err
+	return archiver.Unarchive(file, extractDir)
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/jovandeginste/gr
 go 1.15
 
 require (
+	github.com/golang/snappy v0.0.3 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.0
-	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/mholt/archiver/v3 v3.5.0
 	github.com/sirupsen/logrus v1.8.1
-	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
 	golang.org/x/mod v0.4.2
 )
